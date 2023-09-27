297d5308680a23aebb7c9f8abf0ce98b65e46d52
mka <mka@A22PC5>
2023-09-27 21:02:04 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HttpClientSample
+{
+    public class Car
+    {
+        public string Name;
+        public string VINcode;
+    }
+
+    class Program
+    {
+        static List<Car> list = new List<Car>
+        {
+            new Car() {Name = "Audi", VINcode = "AI2512BT" },
+            new Car() {Name = "BMW", VINcode = "NR2345OW" },
+            new Car() {Name = "Dodge", VINcode = "BK5167RS" }
+        };
+
+        static int port = 8080;
+        static string address = "127.0.0.1"; // localhost
+        static void Main(string[] args)
+        {
+            // створення кінцевої точки для запуску сервера
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+            // створюємо сокет на вказаній кінцевій точці
+            TcpListener listener = new TcpListener(ipPoint);
+
+            // запуск приймання підключень на сервер
+            listener.Start(10);
+
+            while (true)
+            {
+                Console.WriteLine("Server started! Waiting for connection...");
+                
+                TcpClient client = listener.AcceptTcpClient(); // wait until connection
+
+                try
+                {
+                    while (client.Connected)
+                    {
+                        NetworkStream ns = client.GetStream();
+
+                        // отримуємо переданий об'єкт та десеріалізуємо його
+                        //BinaryFormatter formatter = new BinaryFormatter();
+                        //var request = JsonSerializer.Deserialize<string>(ns);
+
+                        StreamReader streamReader= new StreamReader(ns);
+                        
+                        var request = streamReader.ReadLine();
+
+                        Console.WriteLine($"Request data: {request} from {client.Client.RemoteEndPoint}");
+
+                        // відправляємо відповідь
+                        
+                        string response = $"Result = {list.Where(x => x.VINcode == request).Select(x => x.Name).First()}";
+                        Console.WriteLine(response);
+
+                        StreamWriter sw = new StreamWriter(ns); // розмір буфера за замовчуванням: 1KB
+                        sw.WriteLine(response);
+                        sw.Flush(); // clear buffer
+                    }
+
+                    // закриваємо сокет
+                    client.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            listener.Stop();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+
+class Program
+{
+    // адрес та порт сервера, до якого відбувається підключення
+    static int port = 8080;              // порт сервера
+    static string address = "127.0.0.1"; // адреса сервера
+    static string data;
+
+    static void Main(string[] args)
+    {
+        IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+        TcpClient client = new TcpClient();
+
+        // підключення до віддаленого хоста
+        client.Connect(ipPoint);
+
+        try
+        { 
+                // введення даних для відправки
+                Console.Write("Enter a VIN code to get car data >> ");
+                data = Console.ReadLine();
+                // отримуємо потік для обміну повідомленнями
+                NetworkStream ns = client.GetStream();
+                
+                // серіалізація об'єкта та відправка його
+                //JsonSerializer.Serialize(ns, data);
+                StreamWriter streamWriter= new StreamWriter(ns);
+
+                streamWriter.WriteLine(data);
+                streamWriter.Flush();
+
+                // отримуємо відповідь
+                StreamReader sr = new StreamReader(ns);
+                string response = sr.ReadLine();
+
+                Console.WriteLine("Server response: " + response);   
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            // закриваємо підключення
+            client.Close();
+        }
+    }
+}
